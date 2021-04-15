4292c5f9098dc41145129b0bfdaf07c8fd504559
lyddal96 <lyddal96@gmail.com>
2021-04-15 17:32:15 +0900

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,40 +1,4 @@
-install.packages("jsonlite")
 
-library(jsonlite)
-
-install.packages("dplyr ")
-library(dplyr)
-
-install.packages("httr")
-
-library(httr)
-
-df_repos <-fromJSON("http://data.ex.co.kr/openapi/trafficapi/trafficAll?key=3079548252&type=json")
-
-str(df_repos)
-names(df_repos$trafficAll)
-df_repos$trafficAll[1:39,"tmType"]
-
-tf_amout<-df_repos$trafficAll[1:39,"trafficAmout"]
-div_code<-df_repos$trafficAll[1:39,"exDivCode"]
-
-json_text <-fromJSON("C:/users/dkacj/OneDrive/문서/새 폴더/ex3.json")
-
-unitName<-json_text$list[1:156,"unitName"]
-
-print(head(json_text))
-
-filt_data = data.frame(json_text$list)
-filt_data
-print(head(filt_data))
-head(filt_data)
-plot(filt_data$unitCode,filt_data$trafficAmout,xlab= "station", ylab= "amount")
-
-filter(filt_data$unitName~filt_data$trafficAmout)
-
-row.names(filt_data)<-filt_data$unitName~filt_data$trafficAmout
-
-barplot(filt_data$unitName~filt_data$trafficAmout)
 
 install.packages("jsonlite")
 
@@ -43,7 +7,7 @@ library(jsonlite)
 install.packages("dplyr ")
 library(dplyr)
 
-json_text<-fromJSON("C:/Users/dkacj/OneDrive/BigData/traffic/Rocket/export.json")
+json_text<-fromJSON("D://GitHub/Rproject/export.json")
 
 filt <-json_text%>% group_by(unitName)%>%summarize(sum_Amount=sum(as.numeric(trafficAmout)))
 
